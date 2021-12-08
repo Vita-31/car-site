a9672342feb405d9cf4a51ce8ea12ad71d111c27
Vita Kuznetsova <77834576+Vita-31@users.noreply.github.com>
2021-12-08 18:54:08 +0200

    add e.target

--- js/app.js
+++ js/app.js
@@ -2,9 +2,60 @@ const CARS = JSON.parse(DATA);
 
 const dom = {
   feed: document.getElementById('feed'),
+  sortSelect: document.getElementById('sortSelect'),
 };
 
 render(createCardsHTML(CARS), dom.feed);
+getSort()
+
+const sortParams = {
+  top: '',
+  make: '',
+  price: ''
+}
+
+function getSort() {
+  dom.sortSelect.addEventListener('change', (e) => {
+    const select = e.target.classList.contains('sort__select')
+    if(select) {
+      const sortValue = e.target.value.split('/');
+      const key = sortValue[0];
+      const order = sortValue[1];
+      createSort(key, order);
+      // console.log(e.target.value);
+      // sortParams[e.target.name] = e.target.value;
+    }
+  })
+}
+
+// sortSelect.addEventListener('change', e => {
+
+  // console.log(`Change on ${e.target.name}`, e.target.value);
+  // sortParams[e.target.name] = e.target.value
+  // console.log(sortParams);
+// })
+
+
+
+// function getSort() {
+//   const selects = dom.sortSelect.querySelectorAll('.sort__select');
+//   selects.forEach( select => {
+//     select.addEventListener('change', (e) => {
+//       const sortValue = e.target.value.split('/');
+//       const key = sortValue[0];
+//       const order = sortValue[1]
+//       createSort(key, order)
+//     })
+//   })
+// }
+
+function createSort(key, order) {
+  CARS.sort((a, b) => {
+    return String(Number(a[key]) || a[key]).localeCompare(String(Number(b[key]) || b[key]), undefined, {numeric: true}) * order 
+  })
+  render(createCardsHTML(CARS), dom.feed)
+}
+
 
 function render(htmlStr, domElem, insertTo) {
   if (insertTo) {
@@ -18,66 +69,60 @@ function createCardsHTML(cardsArray) {
   return cardsArray.map((cardData) => createCardHTML(cardData)).join('');
 }
 
+
 function createCardHTML(cardData) {
   return `
-      <div class="feed__card">
-        <div class="feed__photo">
-            <img src="${cardData.img}" alt="" width="1" height="1"> 
-            <div class="photo-code">
-                <i class="fas fa-car"></i>
-                ${cardData.vin_check && 'Перевірений VIN-код' || '' }
+      <div class="card">
+        <div class="card__photo">
+            <img src="${cardData.img}" alt="${cardData.make} ${cardData.model} ${cardData.year}" width="1" height="1" loading="lazy" decoding="async"> 
+            <div class="photo__top">
+            ${
+              (cardData.vin_check && `<div class="photo__top-code"><i class="fas fa-car"></i> Перевірено! </div>`) || ''
+            }
+            ${(cardData.top && `<div class="photo__top-top"><i class="fas fa-trophy"> TOP </i></div>`) || ''}
             </div>
-            <div class="photo-top">
-              <i class="fas fa-trophy"></i>
-              ${cardData.top && 'TOP' || '' }
-            </div>
-            <div class="photo-views"> 
-                <i class="far fa-eye"></i>
-                ${cardData.views}
-            </div>
+            ${
+              (cardData.views &&
+                ` <div class="card-views"> 
+            <i class="far fa-eye"></i>${cardData.views} </div>`) ||
+              ''
+            }
         </div>
-        <div class="feed__block">
-            <a href="#!" class="feed-title"> ${cardData.make} ${cardData.model} ${cardData.year} </a>
-            <div class="feed-price">${cardData.price} $</div>
-            <ul class="feed__description">
-                <li class="feed__description-text">
+        <div class="card__block">
+            <a href="#!" class="card-title"> ${cardData.make} ${cardData.model} ${cardData.year} </a>
+            <div class="card-price">${cardData.price} $</div>
+            <ul class="card__description">
+                <li class="card__description-text">
                     <i class="fas fa-tachometer-alt"></i>
                     ${cardData.mileage} км
                 </li>
-                <li class="feed__description-text">
+                <li class="card__description-text">
                     <i class="fas fa-map-marker-alt"></i>
                     ${cardData.country}
                 </li>
-                <li class="feed__description-text">
+                <li class="card__description-text">
                     <i class="fas fa-gas-pump"></i>
                     ${cardData.fuel},  ${cardData.engine_volume}л
                 </li>
-                <li class="feed__description-text">
+                <li class="card__description-text">
                     <i class="fab fa-autoprefixer"></i>
                     ${cardData.transmission}
                 </li>
             </ul>
-            <dl class="feed__code">
-                <dt class="feed__code-title">
-                    Серія:
-                    <dd class="feed__code-number">
-                        <i class="fas fa-tachometer-alt"></i>
-                        ${cardData.series || ' -' }
-                    </dd>
-                </dt>
-                <dt class="feed__code-title">
+            <dl class="card__code">
+                <dt class="card__code-title">
                     Vin-номер:
-                    <dd class="feed__code-number">
+                    <dd class="card__code-number">
                         <i class="fas fa-tachometer-alt"></i>
                         ${cardData.vin || ' -'}
                     </dd>
                 </dt>
             </dl>
-            <dl class="feed__seller">
-              <dt class="feed__seller-name">Продавець: 
+            <dl class="card__seller">
+              <dt class="card__seller-name">Продавець: 
                   <dd> ${cardData.seller || ' -'}</dd>
               </dt> 
-              <dt class="feed__seller-tel">Подзвонити:
+              <dt class="card__seller-tel">Подзвонити:
                 <dd>
                   <a href="tel:${cardData.phone || ' -'}" >
                     <span></span> ${cardData.phone || ' -'}
@@ -85,26 +130,74 @@ function createCardHTML(cardData) {
                 </dd>
               </dt>
             </dl>
-            <h4>Розхід</р>
-            <dl class="feed__consume">
-              
-              <dt class="feed__consume-title">В дорозі: 
-                  <dd class="feed__consume-text">${cardData.road || ' -'}</dd>
-              </dt> 
-              <dt class="feed__consume-title">В місті: 
-                <dd class="feed__consume-text">${cardData.city || ' -'}</dd>
-              </dt>
-              <dt class="feed__consume-title">Змішано:
-                <dd class="feed__consume-text">${cardData.mixed || ' -'}</dd>
-              </dt>
+            <h3>Розхід</h3>
+            <dl class="card__consume">
+              <div>
+                <dt class="card__consume-title">В дорозі:</dt> 
+                <dd class="card__consume-text">${cardData.consume?.road || 'не вказано'} </dd>
+              </div>
+              <div>
+                <dt class="card__consume-title">В місті:</dt>
+                <dd class="card__consume-text">${cardData.consume?.city || 'не вказано'}</dd>
+              </div>
+              <div>
+                <dt class="card__consume-title">Змішано:</dt>
+                <dd class="card__consume-text">${cardData.consume?.mixed || 'не вказано'}</dd>
+              </div>
             </dl>
-            <div class="feed__rating">
-                <div class="rating__stars" id="star">☆☆☆☆☆</div>
-                <p class="rating__number">${cardData.rating}</p>
+            <div class="card__bottom">
+              <div>
+                  <div class="card__stars">${createRating(cardData.rating)}</div>
+              </div>
+              <div class="card__bottom-date">
+                <p>Дата створення оголошення:</p>
+                <p>${new Date(cardData.timestamp).toLocaleDateString()}</p>
+              </div>
             </div>
-            
         </div>
       </div>
-      <div class="line"></div>
     `;
 }
+
+function createRating(rating) {
+  let stars = '';
+  for (let i = 0; i < 5; i++) {
+    if (rating > i + 0.5) {
+      stars += '<i class="fas fa-star"></i>';
+    } else if (rating > i) {
+      stars += '<i class="fas fa-star-half-alt"></i>';
+    } else {
+      stars += '<i class="far fa-star"></i>';
+    }
+  }
+  return stars;
+}
+
+// const array = [5,10,9,12,2,6,7,11,4,3]
+// array.sort((a,b) => {
+//   return String(a).localeCompare(String(b), undefined, {numeric: true})
+// })
+// console.log(array);
+
+// const array = ['ёжик','яблоко','лес']
+// array.sort((a,b) => {
+//   return String(Number(a)).localeCompare(String(Number(b)), undefined, {numeric: true})
+// })
+// console.log(array);
+
+// const array = [true, true, false, true, false, false];
+// array.sort((a, b) => {
+//   return String(Number(a)).localeCompare(String(Number(b)), undefined, { numeric: true });
+// });
+// console.log(array);
+
+// uk-UA
+// ru-UA
+// en-GB
+// en-US
+// en-CA
+
+
+// document.addEventListener('click', e => {
+//   console.log('Click!');
+// })
